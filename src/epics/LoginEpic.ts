ed6489bd7211c837f05ae9d314f2415163f5e344
Sergey Aldoukhov <saldoukhov@gmail.com>
2019-11-08 11:12:05 -0800

    improve epic

--- src/epics/LoginEpic.ts
+++ src/epics/LoginEpic.ts
@@ -1,9 +1,10 @@
 import {Epic} from "redux-observable";
-import {filter, map} from 'rxjs/operators';
+import {filter, map, mergeMap, tap} from 'rxjs/operators';
+import {ajax} from 'rxjs/ajax';
+
 import {ActionType, isActionOf} from 'typesafe-actions';
 
 import * as actions from "../actions";
-
 import {RootState} from "../reducers";
 
 type Action = ActionType<typeof actions>;
@@ -11,7 +12,10 @@ type Action = ActionType<typeof actions>;
 const loginEpic: Epic<Action, Action, RootState> = (action$, store) =>
     action$.pipe(
         filter(isActionOf(actions.loginAction)),
-        map(() => actions.loggedInAction("asd"))
+        tap(console.log),
+        map(x => x.payload.user.split("@")[0]),
+        mergeMap(user => ajax.getJSON<string>(`https://api.github.com/users/${user}`)),
+        map(response => actions.loggedInAction(response))
     );
 
 export default [
